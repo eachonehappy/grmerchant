c634af0fde8f1175080cacda38e12703673dfde9
Aditya Singh <work.asingh@gmail.com>
2017-02-23 17:08:18 +0530

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
 	
  def last_merchant_pin
 
-	(User.where(:admin => false).maximum(:merchant_pin).to_i + 2).to_s.rjust(3, '0')
+	(User.all.count.to_i + 1).to_s.rjust(3, '0')
 end
 
  def last_sku
