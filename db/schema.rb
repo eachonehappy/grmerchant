f8fec3d98a9abb26de7acf4701a0e1a855f863f0
Aditya Singh <work.asingh@gmail.com>
2017-03-16 17:15:22 +0530

    mysql setup

--- db/schema.rb
+++ db/schema.rb
@@ -13,86 +13,83 @@
 
 ActiveRecord::Schema.define(version: 20170227113614) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "cart_recipes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "recipe_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id",    limit: 4
+    t.integer  "recipe_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   add_index "cart_recipes", ["recipe_id"], name: "index_cart_recipes_on_recipe_id", using: :btree
   add_index "cart_recipes", ["user_id"], name: "index_cart_recipes_on_user_id", using: :btree
 
   create_table "customer_addresses", force: :cascade do |t|
-    t.text     "address"
-    t.integer  "customer_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "address",     limit: 65535
+    t.integer  "customer_id", limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   add_index "customer_addresses", ["customer_id"], name: "index_customer_addresses_on_customer_id", using: :btree
 
   create_table "customer_users", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "customer_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "user_id",     limit: 4
+    t.integer  "customer_id", limit: 4
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
   add_index "customer_users", ["customer_id"], name: "index_customer_users_on_customer_id", using: :btree
   add_index "customer_users", ["user_id"], name: "index_customer_users_on_user_id", using: :btree
 
   create_table "customers", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "phone"
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "phone",      limit: 255
   end
 
   create_table "merchant_informations", force: :cascade do |t|
-    t.string   "full_name"
-    t.text     "address"
-    t.string   "phone"
-    t.boolean  "non_veg",     default: true
-    t.boolean  "is_accepted", default: false
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.string   "full_name",   limit: 255
+    t.text     "address",     limit: 65535
+    t.string   "phone",       limit: 255
+    t.boolean  "non_veg",                   default: true
+    t.boolean  "is_accepted",               default: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
   end
 
   create_table "notes_orders", force: :cascade do |t|
-    t.string   "description"
-    t.integer  "order_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "description", limit: 255
+    t.integer  "order_id",    limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   add_index "notes_orders", ["order_id"], name: "index_notes_orders_on_order_id", using: :btree
 
   create_table "order_recipes", force: :cascade do |t|
-    t.integer  "order_id"
-    t.integer  "recipe_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "order_id",   limit: 4
+    t.integer  "recipe_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   add_index "order_recipes", ["order_id"], name: "index_order_recipes_on_order_id", using: :btree
   add_index "order_recipes", ["recipe_id"], name: "index_order_recipes_on_recipe_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "customer_id"
-    t.integer  "customer_address_id"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-    t.integer  "amount"
-    t.string   "mode_of_payment"
-    t.string   "delivery_time"
-    t.string   "o_id"
-    t.boolean  "preorder",            default: false
-    t.string   "sms_status",          default: "default"
+    t.integer  "user_id",             limit: 4
+    t.integer  "customer_id",         limit: 4
+    t.integer  "customer_address_id", limit: 4
+    t.datetime "created_at",                                          null: false
+    t.datetime "updated_at",                                          null: false
+    t.integer  "amount",              limit: 4
+    t.string   "mode_of_payment",     limit: 255
+    t.string   "delivery_time",       limit: 255
+    t.string   "o_id",                limit: 255
+    t.boolean  "preorder",                        default: false
+    t.string   "sms_status",          limit: 255, default: "default"
     t.boolean  "is_delivered"
   end
 
@@ -101,51 +98,51 @@ ActiveRecord::Schema.define(version: 20170227113614) do
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "recipes", force: :cascade do |t|
-    t.string   "sku"
-    t.string   "name"
-    t.integer  "serving"
-    t.integer  "price"
+    t.string   "sku",          limit: 255
+    t.string   "name",         limit: 255
+    t.integer  "serving",      limit: 4
+    t.integer  "price",        limit: 4
     t.boolean  "non_veg"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.boolean  "availability", default: false
-    t.string   "cusine"
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.boolean  "availability",             default: false
+    t.string   "cusine",       limit: 255
   end
 
   create_table "sms_messages", force: :cascade do |t|
-    t.string   "sms_message"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "sms_message", limit: 255
+    t.integer  "user_id",     limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "stats", force: :cascade do |t|
-    t.boolean  "shop_open",  default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.string   "discount"
-    t.boolean  "sms_accept", default: false
-    t.string   "pin"
+    t.boolean  "shop_open",              default: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "discount",   limit: 255
+    t.boolean  "sms_accept",             default: false
+    t.string   "pin",        limit: 255
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "merchant_pin",           default: "",         null: false
-    t.string   "encrypted_password",     default: "",         null: false
-    t.string   "full_name"
-    t.text     "address"
-    t.boolean  "non_veg",                default: true
-    t.string   "reset_password_token"
+    t.string   "merchant_pin",           limit: 255,   default: "",         null: false
+    t.string   "encrypted_password",     limit: 255,   default: "",         null: false
+    t.string   "full_name",              limit: 255
+    t.text     "address",                limit: 65535
+    t.boolean  "non_veg",                              default: true
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,          null: false
+    t.integer  "sign_in_count",          limit: 4,     default: 0,          null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
-    t.text     "phone"
-    t.string   "role",                   default: "merchant"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                                null: false
+    t.datetime "updated_at",                                                null: false
+    t.text     "phone",                  limit: 65535
+    t.string   "role",                   limit: 255,   default: "merchant"
   end
 
   add_index "users", ["merchant_pin"], name: "index_users_on_merchant_pin", unique: true, using: :btree
